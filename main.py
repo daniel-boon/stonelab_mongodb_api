20a85ed2f0737751e308aa62a8a3779c30ad5f7c
daniel-boon <61857025+daniel-boon@users.noreply.github.com>
2021-01-18 13:34:16 +0700

    few update

--- main.py
+++ main.py
@@ -8,40 +8,34 @@ from bson.json_util import dumps
 from bson.json_util import loads
 from connection.settings import Database_connection
 
+app = FastAPI()
+
 #database class
 database = Database_connection()
 client = database.client
 mydb = client['stonelab']
 collection = mydb['customers']
 
-app = FastAPI()
-
-# def check_connection(database):
-#     '''check_connection('database_name') 
-    
-#     to check connection'''
-#     try:
-#         client.server_info()
-#         print('SUCESSFULLY CONNECTED TO -> {}'.format(database))
-#     except:
-#         print('CONNECTION ERROR!')
 
 
+#API calling example
 # http://127.0.0.1:8000/v1/mahistory/?start_date='2020-01-17'&end_date='2020-01-18'
 @app.get('/v1/mahistory/')
 async def ma_history(start_date: str, end_date: str):
     # database.check_connection()
+
+    #use variables from query parameters
     print('start date', start_date)
     print('end date', end_date)
+
     myquery = { "address": "Park Lane 38" }
-
     mydoc = collection.find(myquery)
-
 
     list_cur = list(mydoc)
     result = dumps(list_cur,sort_keys=True,ensure_ascii=True)
     result = json.loads(result.replace("\'", '"'))
 
     return result
+
 
 #uvicorn main:app --reload
